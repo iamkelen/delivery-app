5641daff0da628deef25be51e0d462a0abdcf1bd
iamkelen <gkalashnick@gmail.com>
2020-03-06 15:21:23 +0200

    Add length validations to models

--- app/models/courier.rb
+++ app/models/courier.rb
@@ -1,8 +1,8 @@
 class Courier < ApplicationRecord
   has_many :packages, dependent: :destroy
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 15 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 15 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 end
--- app/models/package.rb
+++ app/models/package.rb
@@ -1,6 +1,6 @@
 class Package < ApplicationRecord
   belongs_to :courier
-  validates :tracking_number, presence: true
+  validates :tracking_number, presence: true, length: { maximum: 15 }
 
   def humanized_delivery_status
     delivery_status ? 'Delivered' : 'Lost'
