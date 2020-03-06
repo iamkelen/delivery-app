8a22c5b88085f057c096370b6944d24a1bf20178
iamkelen <gkalashnick@gmail.com>
2020-03-06 16:24:23 +0200

    Update model validations

--- app/models/courier.rb
+++ app/models/courier.rb
@@ -1,8 +1,8 @@
 class Courier < ApplicationRecord
   has_many :packages, dependent: :destroy
-  validates :name, presence: true, length: { maximum: 15 }
+  validates :name, presence: true, length: { maximum: 60 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 15 },
+  validates :email, presence: true, length: { maximum: 25 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 end
